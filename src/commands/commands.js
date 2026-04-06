42e63b1fd1f3c93dad1b8e495cad78a950e9ea02
DELLASIEGAexternal2 <260647862+DELLASIEGAexternal2@users.noreply.github.com>
2026-04-06 15:47:51 +0200

    Update commands.js

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,34 +1,14 @@
 /* global Office */
 
 Office.onReady(() => {
-  console.log("Herisson ready");
+  console.log("Commands loaded");
 });
 
 function openConfirmDialog(event) {
-  const item = Office.context.mailbox.item;
-  if (!item) {
-    event.completed();
-    return;
-  }
-
-  const dialogUrl = "https://dellasiegaexternal2.github.io/Herisson/src/confirm.html";
-
-  const mailData = {
-    subject: item.subject,
-    sender: item.from?.emailAddress,
-    senderName: item.from?.displayName,
-    date: item.dateTimeCreated
-  };
-
   Office.context.ui.displayDialogAsync(
-    dialogUrl,
+    "https://dellasiegaexternal2.github.io/Herisson/src/confirm.html",
     { height: 70, width: 60 },
-    (asyncResult) => {
-      const dialog = asyncResult.value;
-      setTimeout(() => {
-        dialog.messageChild(JSON.stringify(mailData));
-      }, 300);
-    }
+    () => {}
   );
 
   event.completed();
